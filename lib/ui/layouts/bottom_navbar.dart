969fdabc44a1e8e8f29ce8fc1abdae6e90c288fa
Oliver Boamah <oliverboamah@yahoo.com>
2019-10-20 10:08:23 +0000

    Add shadow to bottom navbar

--- lib/ui/layouts/bottom_navbar.dart
+++ lib/ui/layouts/bottom_navbar.dart
@@ -26,97 +26,101 @@ class _BottomNavBarState extends State<BottomNavBar> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(bottom: 10),
-      color: PRIMARY_COLOR,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          InkWell(
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Icon(
-                    CustomHomeIcon.home,
-                    color: this.selectedTabIndex == 0
-                        ? Colors.grey.shade800
-                        : Colors.white,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8, top: 5),
-                    child: Text(
-                      'Home',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: this.selectedTabIndex == 0
-                              ? Colors.grey.shade800
-                              : Colors.white),
+    return Card(
+      elevation: 12,
+      margin: EdgeInsets.all(0),
+      child: Container(
+        padding: const EdgeInsets.all(4),
+        color: PRIMARY_COLOR,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            InkWell(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Icon(
+                      CustomHomeIcon.home,
+                      color: this.selectedTabIndex == 0
+                          ? Colors.grey.shade800
+                          : Colors.white,
                     ),
-                  )
-                ],
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8, top: 5),
+                      child: Text(
+                        'Home',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: this.selectedTabIndex == 0
+                                ? Colors.grey.shade800
+                                : Colors.white),
+                      ),
+                    )
+                  ],
+                ),
+                decoration: BoxDecoration(
+                    color: this.selectedTabIndex == 0 ? ACCENT_COLOR : null,
+                    borderRadius: BorderRadius.circular(40)),
               ),
-              decoration: BoxDecoration(
-                  color: this.selectedTabIndex == 0 ? ACCENT_COLOR : null,
-                  borderRadius: BorderRadius.circular(40)),
+              onTap: () => this._onItemSelected(0),
             ),
-            onTap: () => this._onItemSelected(0),
-          ),
-          InkWell(
-              child: Container(
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                child: Icon(
-                  CustomHeartIcon.heart,
-                  color: this.selectedTabIndex == 1
-                      ? Colors.grey.shade800
-                      : Colors.white,
-                ),
-                decoration: BoxDecoration(
-                    color: this.selectedTabIndex == 1 ? ACCENT_COLOR : null,
-                    borderRadius: BorderRadius.circular(20)),
-              ),
-              onTap: () => this._onItemSelected(1)),
-          InkWell(
-              child: Container(
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                child: Icon(
-                  CustomEnvelopeIcon.envelope,
-                  color: this.selectedTabIndex == 2
-                      ? Colors.grey.shade800
-                      : Colors.white,
-                ),
-                decoration: BoxDecoration(
-                    color: this.selectedTabIndex == 2 ? ACCENT_COLOR : null,
-                    borderRadius: BorderRadius.circular(20)),
-              ),
-              onTap: () => this._onItemSelected(2)),
-          InkWell(
-              child: Container(
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                child: Badge(
-                  badgeContent: Text(
-                    '${this.widget.notificationsCount}',
-                    style: TextStyle(
-                        color: Colors.grey.shade800,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  badgeColor: ACCENT_COLOR,
-                  position: BadgePosition.topLeft(top: -10, left: -5),
-                  showBadge: this.selectedTabIndex == 3 ? false : true,
+            InkWell(
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
                   child: Icon(
-                    CustomBellIcon.notification,
-                    color: this.selectedTabIndex == 3
+                    CustomHeartIcon.heart,
+                    color: this.selectedTabIndex == 1
                         ? Colors.grey.shade800
                         : Colors.white,
                   ),
+                  decoration: BoxDecoration(
+                      color: this.selectedTabIndex == 1 ? ACCENT_COLOR : null,
+                      borderRadius: BorderRadius.circular(20)),
                 ),
-                decoration: BoxDecoration(
-                    color: this.selectedTabIndex == 3 ? ACCENT_COLOR : null,
-                    borderRadius: BorderRadius.circular(20)),
-              ),
-              onTap: () => this._onItemSelected(3)),
-        ],
+                onTap: () => this._onItemSelected(1)),
+            InkWell(
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: Icon(
+                    CustomEnvelopeIcon.envelope,
+                    color: this.selectedTabIndex == 2
+                        ? Colors.grey.shade800
+                        : Colors.white,
+                  ),
+                  decoration: BoxDecoration(
+                      color: this.selectedTabIndex == 2 ? ACCENT_COLOR : null,
+                      borderRadius: BorderRadius.circular(20)),
+                ),
+                onTap: () => this._onItemSelected(2)),
+            InkWell(
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: Badge(
+                    badgeContent: Text(
+                      '${this.widget.notificationsCount}',
+                      style: TextStyle(
+                          color: Colors.grey.shade800,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    badgeColor: ACCENT_COLOR,
+                    position: BadgePosition.topLeft(top: -10, left: -5),
+                    showBadge: this.selectedTabIndex == 3 ? false : true,
+                    child: Icon(
+                      CustomBellIcon.notification,
+                      color: this.selectedTabIndex == 3
+                          ? Colors.grey.shade800
+                          : Colors.white,
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                      color: this.selectedTabIndex == 3 ? ACCENT_COLOR : null,
+                      borderRadius: BorderRadius.circular(20)),
+                ),
+                onTap: () => this._onItemSelected(3)),
+          ],
+        ),
       ),
     );
   }
